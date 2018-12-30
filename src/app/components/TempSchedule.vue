6f5616cad0e7b8142b937424ac603d3293e0989a
Storm Heg <storm@stormheg.co>
2018-12-30 17:49:00 +0100

    Add vue router

--- src/app/components/TempSchedule.vue
+++ src/app/components/TempSchedule.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="container">
-    <div class="scheduleArea">
-      <!-- ALWAYS include a :key attribute with a unique property (like an id)-->
+  <div class="scheduleArea">
+    <!-- ALWAYS include a :key attribute with a unique property (like an id)-->
+    <div
+      v-for="day in week"
+      :key="day.date"
+      class="dayBox"
+    >
+      <div class="dayTitle">
+        {{ day.date_f }}
+      </div>
       <div
-        v-for="day in week"
-        :key="day.date"
-        class="dayBox"
+        v-for="course in day.items"
+        :key="course.et"
+        class="courseBox"
       >
-        <div class="dayTitle">
-          {{ day.date_f }}
+        <div id="courseTimespan">
+          {{ course.t }}
         </div>
-        <div
-          v-for="course in day.items"
-          :key="course.et"
-          class="courseBox"
-        >
-          <div id="courseTimespan">
-            {{ course.t }}
-          </div>
-          <div id="courseName">
-            {{ course.v }}
-          </div>
-          <div id="courseLecturer">
-            {{ course.l }}
-          </div>
-          <div id="courseRoom">
-            {{ course.r }}
-          </div>
+        <div id="courseName">
+          {{ course.v }}
+        </div>
+        <div id="courseLecturer">
+          {{ course.l }}
+        </div>
+        <div id="courseRoom">
+          {{ course.r }}
         </div>
       </div>
     </div>
