076cde460aa65679603fa673252212d1969328a7
Storm Heg <storm@stormheg.co>
2019-01-09 22:20:47 +0100

    Fix webpack production build

---
+++ config/webpack.development.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map', // This enables source maps for debugging purposes
+  devServer: {
+    contentBase: path.resolve(process.cwd(), 'dist/'),
+    compress: true,
+    open: true,
+    port: 8080,
+  },
+});
