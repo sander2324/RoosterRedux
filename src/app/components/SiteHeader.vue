b152798b93d11bc0f944f531e0bd216e1e1e60dc
sander2324 <43219603+sander2324@users.noreply.github.com>
2019-01-12 22:59:22 +0100

    Added basic design to week selectors

--- src/app/components/SiteHeader.vue
+++ src/app/components/SiteHeader.vue
@@ -3,8 +3,13 @@
     <div class="header__title">
       <h2>Deltion Roosters Redux</h2>
     </div>
-    <div class="header__SearchBox noselect">
-      <div @click="updateWeekNumber(-1)" class="header__weekbtn">Week {{ (weekNum - 1) }}</div>
+    <div class="header__SearchBox">
+      <div
+        class="header__SearchBox__weekbtn noselect"
+        @click="updateWeekNumber(-1)"
+      >
+        &lt;
+      </div>
       <input
         v-model="groupInput"
         class="header__SearchBox__input"
@@ -12,9 +17,19 @@
         placeholder="Vul hier je klas in"
         @keyup.enter="submitGroup"
       >
-      <div @click="updateWeekNumber(1)" class="header__weekbtn">Week {{ (weekNum + 1) }}</div>
+      <div
+        class="header__SearchBox__weekDisplay"
+      >
+        Week {{ weekNum }}
+      </div>
+      <div
+        class="header__SearchBox__weekbtn noselect"
+        @click="updateWeekNumber(1)"
+      >
+        &gt;
+      </div>
     </div>
-    <div class="header__GroupDisplay noselect">
+    <div class="header__GroupDisplay">
       <h3>Rooster voor groep: {{ group }}</h3>
     </div>
   </div>
@@ -34,18 +49,19 @@ export default {
     group() { return this.$store.state.group.toUpperCase(); },
     weekNum() { return this.$store.state.weekNumber; },
   },
+  mounted() {
+    this.$store.dispatch(actions.SET_CURRENT_WEEK_NUMBER);
+  },
   methods: {
     submitGroup() {
       // Make the weekNumber reset to the current one every time the user enter in a new group
       this.$store.commit(mutations.UPDATE_GROUP, this.groupInput);
+      this.groupInput = '';
       this.$store.dispatch(actions.GET_WEEK);
     },
     updateWeekNumber(num) {
       this.$store.dispatch(actions.UPDATE_WEEK_NUMBER, num);
     },
-  },
-  mounted() {
-    this.$store.dispatch(actions.SET_CURRENT_WEEK_NUMBER);
   },
 };
 </script>
