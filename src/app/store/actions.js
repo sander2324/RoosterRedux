422ffa59d97e58368ace739e5ae8294791f6591e
Storm Heg <storm@stormheg.co>
2019-01-13 14:51:35 +0100

    Fix mutation name and typo

--- src/app/store/actions.js
+++ src/app/store/actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import filterdAPI from '../helpers/filterAPIdata';
+import filterAPI from '../helpers/filterAPIdata';
 import * as actions from './actionTypes';
 import * as mutations from './mutationTypes';
 
@@ -10,14 +10,14 @@ export default {
     const weekData = await axios.get(`https://roosters-api.stormheg.co/api/v1/roster?group=${context.state.group}`).catch((err) => {
       console.log(err);
     });
-    context.commit(mutations.UPDATE_WEEK, filterdAPI(weekData.data.data,
+    context.commit(mutations.UPDATE_WEEK, filterAPI(weekData.data.data,
       context.state.weekNumber));
   },
   [actions.SET_CURRENT_WEEK_NUMBER]: async (context) => {
-    context.commit(mutations.CHANGE_WEEK_NUMBER, currentWeekNum());
+    context.commit(mutations.UPDATE_WEEK_NUMBER, currentWeekNum());
   },
   [actions.UPDATE_WEEK_NUMBER]: async (context, payload) => {
-    context.commit(mutations.CHANGE_WEEK_NUMBER, payload);
+    context.commit(mutations.UPDATE_WEEK_NUMBER, payload);
     if (context.state.group !== '') context.dispatch(actions.GET_WEEK);
   },
 };
--- src/app/store/mutations.js
+++ src/app/store/mutations.js
@@ -3,5 +3,5 @@ import * as mutations from './mutationTypes';
 export default {
   [mutations.UPDATE_GROUP]: (state, payload) => { state.group = payload; },
   [mutations.UPDATE_WEEK]: (state, payload) => { state.week = payload; },
-  [mutations.CHANGE_WEEK_NUMBER]: (state, payload) => { state.weekNumber += payload; },
+  [mutations.UPDATE_WEEK_NUMBER]: (state, payload) => { state.weekNumber += payload; },
 };
