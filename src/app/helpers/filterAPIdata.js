7b3f7a9cc6d31c1d99c99d7bd56bc59fb0f12a1e
sander2324 <43219603+sander2324@users.noreply.github.com>
2019-04-19 21:55:10 +0200

    Added error message when the given class is invalid

--- src/app/helpers/filterAPIdata.js
+++ src/app/helpers/filterAPIdata.js
@@ -1,5 +1,7 @@
 // Filter API data to a week and exlcude weekends
-export default (apiArray, weeknum) => {
+export default (apiArrayRaw, weeknum) => {
+  if (apiArrayRaw.error === true) return false;
+  const apiArray = apiArrayRaw.data;
   const currentWeek = [];
   for (let i = 0; i < apiArray.length; i += 1) {
     if (Number(apiArray[i].weeknum) === weeknum && !apiArray[i].date_f.startsWith('z')) {
