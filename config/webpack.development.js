021b88f02b2fe5a3e4c2c3fd37e400fcf747dbe1
sander2324 <43219603+sander2324@users.noreply.github.com>
2019-06-06 15:33:15 +0200

    Updated webpack-dev-server to support vue-router

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -7,6 +7,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map', // This enables source maps for debugging purposes
   devServer: {
+    historyApiFallback: true,
     contentBase: path.resolve(process.cwd(), 'dist/'),
     compress: true,
     open: true,
