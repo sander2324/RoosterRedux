0679641858bfec3d4b5edf2b8adca0333d925205
sander2324 <43219603+sander2324@users.noreply.github.com>
2019-07-03 13:39:38 +0200

    Site URL and title automatically update to the given group

--- src/app/components/SiteHeader.vue
+++ src/app/components/SiteHeader.vue
@@ -44,6 +44,7 @@
 <script>
 import * as actions from '../store/actionTypes';
 import * as mutations from '../store/mutationTypes';
+import { constants } from 'crypto';
 
 export default {
   data() {
@@ -65,16 +66,31 @@ export default {
     if (this.$route.params.group) {
       this.$store.commit(mutations.UPDATE_GROUP, this.$route.params.group);
       this.$store.dispatch(actions.GET_WEEK);
+      this.changeTitle(this.$route.params.group);
     }
   },
   methods: {
+
+    // Change the title of the website to the group that is given.
+    changeTitle(group) {
+      document.title = `RoosterRedux | ${group.toUpperCase()}`;
+    },
+
+    // Change the URL of the address bar without reloading the page
+    changeUrl(group) {
+      window.history.pushState(group, `RoosterRedux | ${group}`, `/${group}`);
+      this.changeTitle(group);
+    },
+
     submitGroup(elem) {
       // Unfocus the input box
       elem.target.blur();
       this.$store.commit(mutations.UPDATE_GROUP, this.groupInput);
       this.groupInput = '';
       this.$store.dispatch(actions.GET_WEEK);
+      this.changeUrl(this.$store.state.group);
     },
+
     updateWeekNumber(num) {
       this.$store.dispatch(actions.UPDATE_WEEK_NUMBER, num);
     },
